d299ea9254a801ca4204d007c2cdb3e63ab7377b
github airy13-skyhi <airy13@icloud.com>
2021-05-04 12:51:52 +0900

    Alamofire、クロージャーhttpRequ

--- Swift5Bokete0/ViewController.swift
+++ Swift5Bokete0/ViewController.swift
@@ -6,14 +6,80 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SDWebImage
+import Photos
+
+
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var odaiImageView: UIImageView!
+    @IBOutlet weak var commentTextView: UITextView!
+    
+    var count = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        commentTextView.layer.cornerRadius = 20.0
+        
+        PHPhotoLibrary.requestAuthorization { (status) in
+            
+            switch(status) {
+            
+            case .authorized:
+                break
+            case .notDetermined:
+                break
+            case .restricted:
+                break
+            case .denied:
+                break
+            case .limited:
+                break
+            @unknown default:
+                break
+            }
+            
+        }
+        
     }
-
-
+    
+    //Your API key: 21455919-94e1aab5f50ee6471d2e442fb
+    
+    func getImages(keyword:String) {
+        
+        let url = "https://pixabay.com/api/?key=21455919-94e1aab5f50ee6471d2e442fb&q=\(keyword)"
+        
+        //Alamofireを使ってhttpリクエストを投げる
+        AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+            
+            switch response.result {
+            
+            
+            case .success:
+                let json:JSON = JSON(response.data as Any)
+                let iamgeString = json["hits"][self.count]["webformatURL"].string
+                
+                
+            case .failure(let error):
+                print(error)
+            }
+            
+        }
+        
+        //帰ってきた値のJSON解析を実行
+        //imageView.imageに貼り付け
+        
+    }
+    
+    
+    
 }
 
